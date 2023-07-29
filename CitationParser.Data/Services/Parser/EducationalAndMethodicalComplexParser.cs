7388484c9c8d4e0b8eefc4154c3036ac7c17a05b
SedovAlexandr <sedovaleksandr808@gmail.com>
2023-07-29 17:48:51 +0300

    сделал парсер для образовательно-методического комплекса и тесты

---
+++ CitationParser.Data/Services/Parser/EducationalAndMethodicalComplexParser.cs
@@ -0,0 +1,225 @@
+﻿using System.Text.RegularExpressions;
+using CitationParser.Data.Model;
+
+namespace CitationParser.Services.Parser;
+
+public class EducationalAndMethodicalComplexParser
+{
+    public static List<University> GetUniversity(string citation)
+    {
+        var universityString = citation.Replace('–', '-').Split(". -")[0].Split('/');
+
+        universityString = universityString[universityString.Length - 1].Split(';');
+
+        if (universityString.Length > 1)
+        {
+            universityString = universityString[universityString.Length - 1].Split(',');
+
+
+            List<University> universitiesList = new List<University>();
+
+            for (int i = 0; i < universityString.Length; i++)
+            {
+                universitiesList.Add(new University() { Name = universityString[i].Trim() });
+            }
+
+            return universitiesList;
+        }
+
+        return null;
+
+    }
+    
+    public static List<City> GetCities(string citation)
+    {
+        List < City > cities = new List<City>();
+
+        var citiesString = citation.Replace('–', '-').Split(". -");
+
+        if (citiesString.Length > 1 && citiesString[1].Contains("изд."))
+        {
+            citiesString = citiesString[2].Split(',');
+        }
+        else if (citiesString.Length > 1)
+        {
+            citiesString = citiesString[1].Split(',');
+        }
+
+        if (citiesString.Length > 1)
+        {
+            citiesString = citiesString[0].Split(';');
+
+            for (int i = 0; i < citiesString.Length; i++)
+            {
+                cities.Add(new City()
+                {
+                    Name = citiesString[i].Split(':')[0].Split('(')[0].Trim().Replace("[", string.Empty).Replace("]", string.Empty),
+                });
+
+                if (citiesString[i].Split('(').Length > 1)
+                {
+                    cities[cities.Count - 1].Country = citiesString[i].Split(':')[0].Split('(')[1].Trim().Replace(")", string.Empty);
+                }
+            }
+        }
+
+        return cities;
+    }
+    
+    public static string GetYear(string citation)
+    {
+        var citiesString = citation.Replace('–', '-').Split(". -");
+
+        if (citiesString.Length > 1 && !citiesString[1].Contains("изд."))
+        {
+            citiesString = citiesString[1].Split(',');
+        }
+        else if (citiesString.Length > 1)
+        {
+            citiesString = citiesString[2].Split(',');
+        }
+        else
+        {
+            return null;
+        }
+        
+        return citiesString[citiesString.Length - 1].Trim().Replace("[", string.Empty).Replace("]", string.Empty);
+        
+    }
+    
+    public static string GetPublishingHouse(string citation)
+    {
+        var publishingHouseString = citation.Replace('–', '-').Split(". -");
+
+        if (publishingHouseString.Length > 1 && publishingHouseString[1].Contains("изд."))
+        {
+            publishingHouseString = publishingHouseString[2].Split(':');
+        }
+        else if (publishingHouseString.Length > 1)
+        {
+            publishingHouseString = publishingHouseString[1].Split(':');
+        }
+        else
+        {
+            return null;
+        }
+        
+        if (publishingHouseString.Length > 1)
+        {
+            publishingHouseString = publishingHouseString[publishingHouseString.Length - 1].Split(',');
+
+            return publishingHouseString[0].Trim().Replace("[", string.Empty).Replace("]", string.Empty);
+        }
+
+        return null;
+    }
+    
+    public static string GetDataStorage(string citation)
+    {
+        var dataStorageString = citation.Replace('–', '-').Split(". -");
+
+        for (int i = 0; i < dataStorageString.Length; i++)
+        {
+            if (dataStorageString[i].Contains("диск"))
+            {
+                return dataStorageString[i].Trim();
+            }
+        }
+
+        return null;
+    }
+    
+    public static string GetComplexNumber(string citation)
+    {
+        var complexNumberString = citation.Replace('–', '-').Split(". -");
+
+        for (int i = 0; i < complexNumberString.Length; i++)
+        {
+            if (complexNumberString[i].Contains("№ ГР"))
+            {
+                return complexNumberString[i].Trim();
+            }
+        }
+
+        return null;
+    }
+    
+    public static string GetInformationAboutPublication(string citation)
+    {
+        var complexNumberString = citation.Replace('–', '-').Split(". -");
+
+        for (int i = 0; i < complexNumberString.Length; i++)
+        {
+            if (complexNumberString[i].Contains("изд."))
+            {
+                return complexNumberString[i].Trim();
+            }
+        }
+
+        return null;
+    }
+    
+    public static string GetCountPages(string citation)
+    {
+        var pagesString = citation.Replace('–', '-').Split(". -");
+
+        for (int i = 0; i < pagesString.Length; i++)
+        {
+            if (Regex.IsMatch(pagesString[i].Trim(), @"^\d+\s(c|с)"))
+            {
+                return pagesString[i].Trim();
+            }
+        }
+
+        return null;
+    }
+    
+    public static string GetRegistrationNumber(string citation)
+    {
+        var registrationNumberString = citation.Replace('–', '-').Split(". -");
+
+        if (registrationNumberString.Length > 1 && !registrationNumberString[registrationNumberString.Length -1].Contains("["))
+        {
+            return registrationNumberString[registrationNumberString.Length - 1].Split('/')[0].Split("от")[0]
+                .Replace("Рег. свид.", String.Empty).Trim();
+        }
+
+        return null;
+    }
+    
+    public static string GetDateOfRegistration(string citation)
+    {
+        var dateOfRegistrationString = citation.Replace('–', '-').Split(". -");
+
+        if (dateOfRegistrationString.Length > 1 && !dateOfRegistrationString[dateOfRegistrationString.Length -1].Contains("["))
+        {
+            return dateOfRegistrationString[dateOfRegistrationString.Length - 1].Split('/')[0].Split("от")[1].Trim();
+        }
+
+        return null;
+    }
+    
+    public static string GetPlaceOfRegistration(string citation)
+    {
+        var placeOfRegistrationString = citation.Replace('–', '-').Split(". -");
+
+        if (placeOfRegistrationString.Length > 1 && !placeOfRegistrationString[placeOfRegistrationString.Length -1].Contains("["))
+        {
+            return placeOfRegistrationString[placeOfRegistrationString.Length - 1].Split('/')[1].Trim();
+        }
+
+        return null;
+    }
+    
+    public static string GetInformation(string citation)
+    {
+        var informationString = citation.Replace('–', '-').Split(". -");
+
+        if (informationString.Length > 1 && informationString[informationString.Length -1].Contains("["))
+        {
+            return informationString[informationString.Length - 1].Trim();
+        }
+
+        return null;
+    }
+}
---
+++ CitationParser.Test/Data/Services/Parser/EducationalAndMethodicalComplexTest.cs
@@ -0,0 +1,251 @@
+﻿using CitationParser.Data.Model;
+using CitationParser.Services.Parser;
+
+namespace CitationParser.Test.Data.Services.Parser;
+
+public class EducationalAndMethodicalComplexTest
+{
+    [Fact]
+    public void GetUniversities_SimpleTest()
+    {
+        var citation = "Детали машин и основы конструирования [Электронный ресурс] : электрон. учеб.-метод. комплекс / М.М. Матлин, С.Ю. Кислов, И.М. Шандыбина ; ВолгГТУ. – Волгоград, 2011. – 1 электрон. опт. диск (CD-ROM). – № ГР 0321100012. – Рег. свид. № 21083 от 13 января 2011 г. / ФГУП НТЦ «Информрегистр»";
+
+        List<University> universities = new List<University>();
+        universities.Add(new University(){Name = "ВолгГТУ"});
+
+        var result = EducationalAndMethodicalComplexParser.GetUniversity(citation);
+
+        Assert.Equal(universities.Count, result.Count);
+        for (int i = 0; i < universities.Count; i++)
+        {
+            Assert.Equal(universities[i].Name, result[i].Name);
+        }
+    }
+    
+    [Fact]
+    public void GetUniversities_TwoUniversityTest()
+    {
+        var citation = "Профессиональная межкультурная коммуникация: английский язык : учебно-методический комплекс (Рек. УМО по образованию в области лингвистики в качестве УМК) / Т.Н. Астафурова ; ФГБОУ ВПО «Волгоградский гос. ун-т», Ин-т дополнительного образования. – 3-е изд., доп. – Волгоград : Изд-во ВолГУ, 2011. – 200 с. – [Усл.-печ. л. 11,6 ; тираж 100]";
+
+        List<University> universities = new List<University>();
+        universities.Add(new University(){Name = "ФГБОУ ВПО «Волгоградский гос. ун-т»"});
+        universities.Add(new University(){Name = "Ин-т дополнительного образования"});
+        
+        var result = EducationalAndMethodicalComplexParser.GetUniversity(citation);
+
+        Assert.Equal(universities.Count, result.Count);
+        for (int i = 0; i < universities.Count; i++)
+        {
+            Assert.Equal(universities[i].Name, result[i].Name);
+        }
+    }
+    
+    [Fact]
+    public void GetCities_SimpleTest()
+    {
+        var citation = "Детали машин и основы конструирования [Электронный ресурс] : электрон. учеб.-метод. комплекс / М.М. Матлин, С.Ю. Кислов, И.М. Шандыбина ; ВолгГТУ. – Волгоград, 2011. – 1 электрон. опт. диск (CD-ROM). – № ГР 0321100012. – Рег. свид. № 21083 от 13 января 2011 г. / ФГУП НТЦ «Информрегистр»";
+
+        List<City> cities = new List<City>();
+        cities.Add(new City() {Name = "Волгоград"});
+
+        var result = EducationalAndMethodicalComplexParser.GetCities(citation);
+
+        Assert.Equal(cities.Count, result.Count);
+        for (int i = 0; i < cities.Count; i++)
+        {
+            Assert.Equal(cities[i].Name, result[i].Name);
+            Assert.Equal(cities[i].Country, result[i].Country);
+        }
+    }
+    
+    [Fact]
+    public void GetCities_NullTest()
+    {
+        var citation = "Оценка влияния дорожных условий на пропускную способность городских дорог в зимнее время / Ф.В. Волченко ";
+
+        List<City> cities = new List<City>();
+
+        var result = EducationalAndMethodicalComplexParser.GetCities(citation);
+
+        Assert.Equal(cities.Count, result.Count);
+        for (int i = 0; i < cities.Count; i++)
+        {
+            Assert.Equal(cities[i].Name, result[i].Name);
+            Assert.Equal(cities[i].Country, result[i].Country);
+        }
+    }
+
+    [Fact]
+    public void GetCities_CityIsNotContainedAfterFirstDotAndHyphenTest()
+    {
+        var citation = " Профессиональная межкультурная коммуникация: английский язык : учебно-методический комплекс (Рек. УМО по образованию в области лингвистики в качестве УМК) / Т.Н. Астафурова ; ФГБОУ ВПО «Волгоградский гос. ун-т», Ин-т дополнительного образования. – 3-е изд., доп. – Волгоград : Изд-во ВолГУ, 2011. – 200 с. – [Усл.-печ. л. 11,6 ; тираж 100]";
+
+        List<City> cities = new List<City>();
+        cities.Add(new City(){Name = "Волгоград"});
+
+        var result = EducationalAndMethodicalComplexParser.GetCities(citation);
+
+        Assert.Equal(cities.Count, result.Count);
+        for (int i = 0; i < cities.Count; i++)
+        {
+            Assert.Equal(cities[i].Name, result[i].Name);
+            Assert.Equal(cities[i].Country, result[i].Country);
+        }
+    }
+    
+    [Fact]
+    public void GetYear_SimpleTest()
+    {
+        var citation = "Детали машин и основы конструирования [Электронный ресурс] : электрон. учеб.-метод. комплекс / М.М. Матлин, С.Ю. Кислов, И.М. Шандыбина ; ВолгГТУ. – Волгоград, 2011. – 1 электрон. опт. диск (CD-ROM). – № ГР 0321100012. – Рег. свид. № 21083 от 13 января 2011 г. / ФГУП НТЦ «Информрегистр»";
+
+        var expected = "2011";
+
+        var result = EducationalAndMethodicalComplexParser.GetYear(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetYear_NullTest()
+    {
+        var citation = "Оценка влияния дорожных условий на пропускную способность городских дорог в зимнее время / Ф.В. Волченко ";
+
+        string expected = null;
+
+        var result = EducationalAndMethodicalComplexParser.GetYear(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetYear_YearIsNotContainedAfterFirstDotAndHyphenTest()
+    {
+        var citation = "Профессиональная межкультурная коммуникация: английский язык : учебно-методический комплекс (Рек. УМО по образованию в области лингвистики в качестве УМК) / Т.Н. Астафурова ; ФГБОУ ВПО «Волгоградский гос. ун-т», Ин-т дополнительного образования. – 3-е изд., доп. – Волгоград : Изд-во ВолГУ, 2011. – 200 с. – [Усл.-печ. л. 11,6 ; тираж 100]";
+
+        var expected = "2011";
+
+        var result = EducationalAndMethodicalComplexParser.GetYear(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetPublishingHouse_SimpleTest()
+    {
+        var citation = "Профессиональная межкультурная коммуникация: английский язык : учебно-методический комплекс (Рек. УМО по образованию в области лингвистики в качестве УМК) / Т.Н. Астафурова ; ФГБОУ ВПО «Волгоградский гос. ун-т», Ин-т дополнительного образования. – 3-е изд., доп. – Волгоград : Изд-во ВолГУ, 2011. – 200 с. – [Усл.-печ. л. 11,6 ; тираж 100]";
+
+        var expected = "Изд-во ВолГУ";
+
+        var result = EducationalAndMethodicalComplexParser.GetPublishingHouse(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetPublishingHouse_NullTest()
+    {
+        var citation = "Оценка влияния дорожных условий на пропускную способность городских дорог в зимнее время / Ф.В. Волченко ";
+
+        string expected = null;
+
+        var result = EducationalAndMethodicalComplexParser.GetPublishingHouse(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetDataStorage_SimpleTest()
+    {
+        var citation = "Детали машин и основы конструирования [Электронный ресурс] : электрон. учеб.-метод. комплекс / М.М. Матлин, С.Ю. Кислов, И.М. Шандыбина ; ВолгГТУ. – Волгоград, 2011. – 1 электрон. опт. диск (CD-ROM). – № ГР 0321100012. – Рег. свид. № 21083 от 13 января 2011 г. / ФГУП НТЦ «Информрегистр»";
+
+        var expected = "1 электрон. опт. диск (CD-ROM)";
+
+        var result = EducationalAndMethodicalComplexParser.GetDataStorage(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetComplexNumber_SimpleTest()
+    {
+        var citation = " Детали машин и основы конструирования [Электронный ресурс] : электрон. учеб.-метод. комплекс / М.М. Матлин, С.Ю. Кислов, И.М. Шандыбина ; ВолгГТУ. – Волгоград, 2011. – 1 электрон. опт. диск (CD-ROM). – № ГР 0321100012. – Рег. свид. № 21083 от 13 января 2011 г. / ФГУП НТЦ «Информрегистр»";
+
+        var expected = "№ ГР 0321100012";
+
+        var result = EducationalAndMethodicalComplexParser.GetComplexNumber(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetInformationAboutPublication_SimpleTest()
+    {
+        var citation = "Профессиональная межкультурная коммуникация: английский язык : учебно-методический комплекс (Рек. УМО по образованию в области лингвистики в качестве УМК) / Т.Н. Астафурова ; ФГБОУ ВПО «Волгоградский гос. ун-т», Ин-т дополнительного образования. – 3-е изд., доп. – Волгоград : Изд-во ВолГУ, 2011. – 200 с. – [Усл.-печ. л. 11,6 ; тираж 100]";
+
+        var expected = "3-е изд., доп";
+
+        var result = EducationalAndMethodicalComplexParser.GetInformationAboutPublication(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetCountPages_SimpleTest()
+    {
+        var citation = "Профессиональная межкультурная коммуникация: английский язык : учебно-методический комплекс (Рек. УМО по образованию в области лингвистики в качестве УМК) / Т.Н. Астафурова ; ФГБОУ ВПО «Волгоградский гос. ун-т», Ин-т дополнительного образования. – 3-е изд., доп. – Волгоград : Изд-во ВолГУ, 2011. – 200 с. – [Усл.-печ. л. 11,6 ; тираж 100]";
+
+        var expected = "200 с";
+
+        var result = EducationalAndMethodicalComplexParser.GetCountPages(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetRegistrationNumber_SimpleTest()
+    {
+        var citation = " Детали машин и основы конструирования [Электронный ресурс] : электрон. учеб.-метод. комплекс / М.М. Матлин, С.Ю. Кислов, И.М. Шандыбина ; ВолгГТУ. – Волгоград, 2011. – 1 электрон. опт. диск (CD-ROM). – № ГР 0321100012. – Рег. свид. № 21083 от 13 января 2011 г. / ФГУП НТЦ «Информрегистр»";
+
+        var expected = "№ 21083";
+
+        var result = EducationalAndMethodicalComplexParser.GetRegistrationNumber(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetDateOfRegistration_SimpleTest()
+    {
+        var citation = " Детали машин и основы конструирования [Электронный ресурс] : электрон. учеб.-метод. комплекс / М.М. Матлин, С.Ю. Кислов, И.М. Шандыбина ; ВолгГТУ. – Волгоград, 2011. – 1 электрон. опт. диск (CD-ROM). – № ГР 0321100012. – Рег. свид. № 21083 от 13 января 2011 г. / ФГУП НТЦ «Информрегистр»";
+
+        var expected = "13 января 2011 г.";
+
+        var result = EducationalAndMethodicalComplexParser.GetDateOfRegistration(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetPlaceOfRegistration_SimpleTest()
+    {
+        var citation = " Детали машин и основы конструирования [Электронный ресурс] : электрон. учеб.-метод. комплекс / М.М. Матлин, С.Ю. Кислов, И.М. Шандыбина ; ВолгГТУ. – Волгоград, 2011. – 1 электрон. опт. диск (CD-ROM). – № ГР 0321100012. – Рег. свид. № 21083 от 13 января 2011 г. / ФГУП НТЦ «Информрегистр»";
+
+        var expected = "ФГУП НТЦ «Информрегистр»";
+
+        var result = EducationalAndMethodicalComplexParser.GetPlaceOfRegistration(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetInformation_SimpleTest()
+    {
+        var citation = "Профессиональная межкультурная коммуникация: английский язык : учебно-методический комплекс (Рек. УМО по образованию в области лингвистики в качестве УМК) / Т.Н. Астафурова ; ФГБОУ ВПО «Волгоградский гос. ун-т», Ин-т дополнительного образования. – 3-е изд., доп. – Волгоград : Изд-во ВолГУ, 2011. – 200 с. – [Усл.-печ. л. 11,6 ; тираж 100]";
+
+        var expected = "[Усл.-печ. л. 11,6 ; тираж 100]";
+
+        var result = EducationalAndMethodicalComplexParser.GetInformation(citation);
+        
+        Assert.Equal(expected, result);
+    }
+}
