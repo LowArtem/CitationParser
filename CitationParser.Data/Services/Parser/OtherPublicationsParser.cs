585b5657f20390b20c5db39a6b8c9df90d3978ba
SedovAlexandr <sedovaleksandr808@gmail.com>
2023-07-29 11:17:41 +0300

    закончил парсер для других публикаций и тесты к нему

---
+++ CitationParser.Data/Services/Parser/OtherPublicationsParser.cs
@@ -0,0 +1,185 @@
+﻿using System.Text.RegularExpressions;
+using CitationParser.Data.Model;
+using MoreLinq;
+
+namespace CitationParser.Services.Parser;
+
+public class OtherPublicationsParser
+{
+    public static string GetTitleSource(string citation)
+    {
+        var scientificCollection = citation.Replace('–', '-').Split("//")[1].Split(". -")[0];
+
+        return scientificCollection.Split('/')[0].Trim();
+    }
+    
+    public static string GetYear(string citation)
+    {
+        var citiesString = citation.Replace('–', '-').Split(". -");
+
+        if (!citiesString[1].Contains("Введ."))
+        {
+            citiesString = citiesString[1].Split(',');
+        }
+        else
+        {
+            citiesString = citiesString[2].Split(',');
+        }
+        
+        return citiesString[citiesString.Length - 1].Trim().Replace("[", string.Empty).Replace("]", string.Empty);
+        
+    }
+    
+    public static List<City> GetCities(string citation)
+    {
+        List < City > cities = new List<City>();
+
+        var citiesString = citation.Replace('–', '-').Split(". -");
+
+        if (citiesString[1].Contains("Введ."))
+        {
+            citiesString = citiesString[2].Split(',');
+        }
+        else
+        {
+            citiesString = citiesString[1].Split(',');
+        }
+
+        if (citiesString.Length > 1)
+        {
+            citiesString = citiesString[0].Split(';');
+
+            for (int i = 0; i < citiesString.Length; i++)
+            {
+                cities.Add(new City()
+                {
+                    Name = citiesString[i].Split(':')[0].Split('(')[0].Trim().Replace("[", string.Empty).Replace("]", string.Empty),
+                });
+
+                if (citiesString[i].Split('(').Length > 1)
+                {
+                    cities[cities.Count - 1].Country = citiesString[i].Split(':')[0].Split('(')[1].Trim().Replace(")", string.Empty);
+                }
+            }
+        }
+
+        return cities;
+    }
+    
+    public static string GetPublishingHouse(string citation)
+    {
+        var publishingHouseString = citation.Replace('–', '-').Split(". -")[1].Split(':');
+
+        publishingHouseString = publishingHouseString[publishingHouseString.Length - 1].Split(',');
+
+        if (!int.TryParse(publishingHouseString[0].Trim(), out int n))
+        {
+            return publishingHouseString[0].Trim().Replace("[", string.Empty).Replace("]", string.Empty);
+        }
+
+        return null;
+    }
+
+    public static string GetPagesNumbers(string citation)
+    {
+        var pagesString = citation.Replace('–', '-').Split("//")[1].Split(". -");
+
+        for (int i = 0; i < pagesString.Length; i++)
+        {
+            if (Regex.IsMatch(pagesString[i].Split(';')[0].Trim(), @"^(P|С)\.\s?\d+-?\d?"))
+            {
+                return pagesString[i].Split(';')[0].Trim();
+            }
+        }
+
+        return null;
+    }
+    
+    public static string GetCountPages(string citation)
+    {
+        var pagesString = citation.Replace('–', '-').Split("//")[1].Split(". -");
+
+        for (int i = 0; i < pagesString.Length; i++)
+        {
+            if (Regex.IsMatch(pagesString[i].Trim(), @"^\d+\s(p|с)"))
+            {
+                return pagesString[i].Trim();
+            }
+        }
+
+        return null;
+    }
+    
+    public static string GetURL(string citation)
+    {
+        var URLString = citation.Replace('–', '-').Split(". -");
+
+        for (int i = 0; i < URLString.Length; i++)
+        {
+            if (URLString[i].Contains("URL:")  || URLString[i].Contains("Режим доступа"))
+            {
+                return URLString[i].Trim();
+            }
+        }
+
+        return null;
+    }
+    
+    public static string GetNumber(string citation)
+    {
+        var articleNumberString = citation.Replace('–', '-').Split(". -");
+
+        for (int i = 0; i < articleNumberString.Length; i++)
+        {
+            if (articleNumberString[i].Contains("№") || articleNumberString[i].Contains("Paper No"))
+            {
+                return articleNumberString[i].Trim();
+            }
+        }
+
+        return null;
+    }
+
+    public static List<University> GetUniversity(string citation)
+    {
+        var universityString = citation.Replace('–', '-').Split("//");
+        
+        if (universityString.Length > 1)
+        {
+            universityString = universityString[1].Split(". -")[0].Split('/');
+        }
+        else
+        {
+            universityString = universityString[0].Split(". -")[0].Split('/');
+        }
+
+        universityString = universityString[universityString.Length - 1].Split(';');
+        
+        universityString = universityString[universityString.Length - 1].Split(',');
+        
+        List<University> universitiesList = new List<University>();
+
+        for (int i = 0; i < universityString.Length; i++)
+        {
+            if (!universityString[i].Contains("сост.:"))
+            {
+                universitiesList.Add(new University() { Name = universityString[i].Trim() });
+            }
+        }
+        
+        return universitiesList;
+
+    }
+    
+    public static string GetDateIntroduction(string citation)
+    {
+        var citiesString = citation.Replace('–', '-').Split(". -");
+
+        if (citiesString[1].Contains("Введ."))
+        {
+            return citiesString[1].Trim();
+        }
+
+        return null;
+    }
+}
---
+++ CitationParser.Test/Data/Services/Parser/OtherPublicationsParserTest.cs
@@ -0,0 +1,216 @@
+﻿using CitationParser.Data.Model;
+using CitationParser.Services.Parser;
+
+namespace CitationParser.Test.Data.Services.Parser;
+
+public class OtherPublicationsParserTest
+{
+    [Fact]
+    public void GetTitleSource_SimpleTest()
+    {
+        var citation = "Вячеслав Дербишер. Стихи («Всё меньше ветеранов я встречаю»...) / В.Е. Дербишер // Георгиевская лента : 75 лет Победы. Книга десятая / Российский союз писателей. – Москва, 2020. – С. 18-29";
+
+        var expected = "Георгиевская лента : 75 лет Победы. Книга десятая";
+
+        var result = OtherPublicationsParser.GetTitleSource(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetTitleSource_NotRightSlashTest()
+    {
+        var citation = "«За боль и быль несбывшейся судьбы...» / Ю.В. Артюхович // Отчий край. – 2021. – № 1 (109). – С. 176-181";
+
+        var expected = "Отчий край";
+
+        var result = OtherPublicationsParser.GetTitleSource(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetYear_SimpleTest()
+    {
+        var citation = "Документальное обеспечение стрительства : справочное пособие / В.Н. Кабанов . - Москва : ООО \"Проспект\", 2021. - 144 с";
+
+        var expected = "2021";
+
+        var result = OtherPublicationsParser.GetYear(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetYear_SquareBracketsTest()
+    {
+        var citation = "Трактор-легенда. 70 лет ДТ-54 / В.В. Косенко, В.В. Шаров, Ю.С. Ценч // www.techstory.ru. Сайт о механических экскаваторах, старой строительной, авто- и железнодорожной технике. Сайт об истории отечественной дорожной и строительной техники. Раздел «Тематические статьи». История отечественного тракторостроения. – [2019]. – URL : http://www.techstory.ru/fin/85_dt54_2019.htm";
+
+        var expected = "2019";
+
+        var result = OtherPublicationsParser.GetYear(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetCities_SimpleTest()
+    {
+        var citation = "Вячеслав Дербишер. Стихи («Всё меньше ветеранов я встречаю»...) / В.Е. Дербишер // Георгиевская лента : 75 лет Победы. Книга десятая / Российский союз писателей. – Москва, 2020. – С. 18-29";
+        
+        List<City> cities = new List<City>();
+        cities.Add(new City() {Name = "Москва"});
+
+        var result = OtherPublicationsParser.GetCities(citation);
+
+        Assert.Equal(cities.Count, result.Count);
+        for (int i = 0; i < cities.Count; i++)
+        {
+            Assert.Equal(cities[i].Name, result[i].Name);
+        }
+    }
+    
+    [Fact]
+    public void GetCities_EnglishTest()
+    {
+        var citation = "Blockchain and the institutional complexity: post-institutionalism vs. neoinstitutionalism / Д.П. Фролов // MPRA: Munich Personal RePEc Archive : [on-line publications] / Munich University Library. – Munich (Germany), 2019. – Paper No. 95963 (posted 13 September, 2019). – 18 p. – URL : https://mpra.ub.uni-muenchen.de/95963/1/MPRA_paper_95963.pdf";
+        
+        List<City> cities = new List<City>();
+        cities.Add(new City() {Name = "Munich", Country = "Germany"});
+
+        var result = OtherPublicationsParser.GetCities(citation);
+
+        Assert.Equal(cities.Count, result.Count);
+        for (int i = 0; i < cities.Count; i++)
+        {
+            Assert.Equal(cities[i].Name, result[i].Name);
+            Assert.Equal(cities[i].Country, result[i].Country);
+        }
+    }
+    
+    [Fact]
+    public void GetPublishingHouse_SimpleTest()
+    {
+        var citation = "Добрые стихи. Стихи для детей / Ю.В. Артюхович ; художник: Е. Г. Новикова ; худож. редактор: А. Ю. Шурыгина ; [Комитет культуры Волгоградской обл.]. – Волгоград : ГБУ культуры «Издатель», 2020. – 27 с";
+
+        var expected = "ГБУ культуры «Издатель»";
+
+        var result = OtherPublicationsParser.GetPublishingHouse(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetPages_SimpleTest()
+    {
+        var citation = "Женская территория / Ю.В. Артюхович // IV международная литературная премия «Перископ-2020» : литературный конкурс. – Волгоград, 2021. – С. 275-289. – URL: http://periscope-volga.ru/news/itogi_iv_mezhdunarodnoj_literaturnoj_premii_periskop_2020/2020-12-08-183";
+
+        var expected = "С. 275-289";
+
+        var result = OtherPublicationsParser.GetPagesNumbers(citation);
+        
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void GetPages_OnePageTest()
+    {
+        var citation = "Применение кинетического метода расчёта многокомпонентной изотермической абсорбции / Н.Г. Неумоина, А.В. Белов // Научное обозрение. Технические науки : [обзор статей, опубликованных в журнале «Современные проблемы науки и образования» по техническим наукам в 2013 году] / РАЕ. – 2014. – № 2. – С. 76 ; парал.: рус., англ.";
+
+        var expected = "С. 76";
+
+        var result = OtherPublicationsParser.GetPagesNumbers(citation);
+
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetCountPages_SimpleTest()
+    {
+        var citation = "Факторы влияющие на обработку титановых сплавов с опережающим пластическим деформированием [Электронный ресурс] : доклад / И.Н. Козачухненко, Д.В. Крайнев, Н.Г. Сахнов // Практическое значение современных научных исследований ‘2017 : междунар. науч.-практ. Интернет-конф. (13-20 июня 2017 г.). Секция «Технические науки», подсекция «Машиноведение и машиностроение» / Научно-издательский проект «Scientific World» (SWorld). – [Одесса], 2017. – 6 с. – Режим доступа : http://www.sworld.education/index.php/ru/technical-sciences-ua217/machines-and-mechanical-engineering-ua217/29285-ua217-028";
+
+        var expected = "6 с";
+
+        var result = OtherPublicationsParser.GetCountPages(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetCountPages_EnglishTest()
+    {
+        var citation = "Blockchain and the institutional complexity: post-institutionalism vs. neoinstitutionalism / Д.П. Фролов // MPRA: Munich Personal RePEc Archive : [on-line publications] / Munich University Library. – Munich (Germany), 2019. – Paper No. 95963 (posted 13 September, 2019). – 18 p. – URL : https://mpra.ub.uni-muenchen.de/95963/1/MPRA_paper_95963.pdf";
+
+        var expected = "18 p";
+
+        var result = OtherPublicationsParser.GetCountPages(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetURL_SimpleTest()
+    {
+        var citation = "Женская территория / Ю.В. Артюхович // IV международная литературная премия «Перископ-2020» : литературный конкурс. – Волгоград, 2021. – С. 275-289. – URL: http://periscope-volga.ru/news/itogi_iv_mezhdunarodnoj_literaturnoj_premii_periskop_2020/2020-12-08-183";
+
+        var expected = "URL: http://periscope-volga.ru/news/itogi_iv_mezhdunarodnoj_literaturnoj_premii_periskop_2020/2020-12-08-183";
+
+        var result = OtherPublicationsParser.GetURL(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetNumber_SimpleTest()
+    {
+        var citation = " Blockchain and the institutional complexity: post-institutionalism vs. neoinstitutionalism / Д.П. Фролов // MPRA: Munich Personal RePEc Archive : [on-line publications] / Munich University Library. – Munich (Germany), 2019. – Paper No. 95963 (posted 13 September, 2019). – 18 p. – URL : https://mpra.ub.uni-muenchen.de/95963/1/MPRA_paper_95963.pdf";
+
+        var expected = "Paper No. 95963 (posted 13 September, 2019)";
+
+        var result = OtherPublicationsParser.GetNumber(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetNumber_SecondTest()
+    {
+        var citation = "«За боль и быль несбывшейся судьбы...» / Ю.В. Артюхович // Отчий край. – 2021. – № 1 (109). – С. 176-181";
+
+        var expected = "№ 1 (109)";
+
+        var result = OtherPublicationsParser.GetNumber(citation);
+        
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetUniversities_SimpleTest()
+    {
+        var citation = "Blockchain and the institutional complexity: post-institutionalism vs. neoinstitutionalism / Д.П. Фролов // MPRA: Munich Personal RePEc Archive : [on-line publications] / Munich University Library. – Munich (Germany), 2019. – Paper No. 95963 (posted 13 September, 2019). – 18 p. – URL : https://mpra.ub.uni-muenchen.de/95963/1/MPRA_paper_95963.pdf";
+
+        List<University> universities = new List<University>();
+        universities.Add(new University(){Name = "Munich University Library"});
+
+        var result = OtherPublicationsParser.GetUniversity(citation);
+
+        Assert.Equal(universities.Count, result.Count);
+        for (int i = 0; i < universities.Count; i++)
+        {
+            Assert.Equal(universities[i].Name, result[i].Name);
+        }
+    }
+
+    [Fact]
+    public void GetDateIntroduction_SimpleTest()
+    {
+        var citation =
+            "Проектирование земляного полотна автомобильных дорог в сложных геологических, гидрологических и климатических условиях горной части республики Дагестан : СТО.25106343.01-2014.РМД : рег. метод. док. / С.В. Алексиков, А.И. Лескин, Д.И. Гофман ; Агентство по трансп. и дорож. хоз-ву респ. Дагестан. - Введ. 03 апреля 2015 г. - Махачкала, 2015. - 86 с. - (Стандарт организации)";
+        
+        var expected = "Введ. 03 апреля 2015 г";
+
+        var result = OtherPublicationsParser.GetDateIntroduction(citation);
+        
+        Assert.Equal(expected, result);
+    }
+}
