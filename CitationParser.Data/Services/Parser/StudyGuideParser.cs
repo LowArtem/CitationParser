0d8c67eb492325d8d6697d611ab74512f8c6dd6a
SedovAlexandr <sedovaleksandr808@gmail.com>
2023-09-13 15:26:42 +0300

    Реализовал для парсера учебного пособия функции получения университетов и редакторов и тесты к ним

---
+++ CitationParser.Data/Services/Parser/StudyGuideParser.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+using CitationParser.Data.Model;
+
+namespace CitationParser.Services.Parser;
+
+public class StudyGuideParser
+{
+    public static List<Editor> GetEditor(string citation)
+    {
+        var editorString = citation.Replace('–', '-').Split(". -")[0].Split('/');
+
+        if (editorString.Length > 1)
+        {
+            editorString = editorString[1].Split(';');
+        }
+
+        if (editorString.Length > 1 && editorString[1].Contains("ред"))
+        {
+            var editors = Regex.Matches(editorString[1], @"\w*\s?[ЁА-Я]{1}\.(\s?[ЁА-Я]{1}\.)?\s?\w*,?");
+
+            List<Editor> editorsList = new List<Editor>();
+            for (int i = 0; i < editors.Count; i++)
+            {
+                editorsList.Add(new Editor() { Name = editors[i].ToString().Replace(",", string.Empty).Trim() });
+            }
+
+            return editorsList;
+
+        }
+
+        return null;
+    }
+
+    public static List<University> GetUniversity(string citation)
+    {
+        var universityString = citation.Replace('–', '-').Split(". -")[0].Split('/');
+
+        universityString = universityString[universityString.Length - 1].Split(';');
+
+        if (!universityString[universityString.Length - 1].Contains("ред."))
+        {
+            universityString = universityString[universityString.Length - 1].Split(',');
+
+            List<University> universitiesList = new List<University>();
+
+            for (int i = 0; i < universityString.Length; i++)
+            {
+                universitiesList.Add(new University() { Name = universityString[i].Trim().Replace("[", string.Empty).Replace("]", string.Empty) });
+            }
+
+            return universitiesList;
+        }
+
+        return null;
+
+    }
+}
---
+++ CitationParser.Test/Data/Services/Parser/StudyGuideParserTest.cs
@@ -0,0 +1,92 @@
+﻿using CitationParser.Data.Model;
+using CitationParser.Services.Parser;
+
+namespace CitationParser.Test.Data.Services.Parser;
+
+public class StudyGuideParserTest
+{
+    [Fact]
+    public void GetEditor_SimpleTest()
+    {
+        var citation = "Тепломассообмен: учеб. пособие / В.С. Кузеванов, Г.С. Закожурникова, С.С. Закожурников; под ред. В. С. Кузеванова. - Москва, 2022. - 193 с.";
+
+        List<Editor> editors = new List<Editor>();
+        editors.Add(new Editor(){Name = "В. С. Кузеванова"});
+
+        var result = StudyGuideParser.GetEditor(citation);
+        
+        Assert.Equal(editors.Count, result.Count);
+        for (int i = 0; i < editors.Count; i++)
+        {
+            Assert.Equal(editors[i].Name, result[i].Name);
+        }
+    }
+    
+    [Fact]
+    public void GetEditor_NullTest()
+    {
+        var citation = "Мир стритбола: учеб. пособие / И.Н. Прытков, Д.В. Николаев, Н.И. Грицак; КТИ (филиал) ВолгГТУ. - Волгоград, 2022. - 88 с.";
+
+        var result = StudyGuideParser.GetEditor(citation);
+        
+        Assert.Null(result);
+    }
+    
+    [Fact]
+    public void GetEditor_PublicationsContainsUniversityTest()
+    {
+        var citation = "Надежность машин (теория и практика): учеб. пособие / С.А. Крюков, В.А. Граблин, Н.В. Байдакова; под ред. В. М. Шумячера ; ВПИ (филиал) ФГБОУ ВО ВолгГТУ. - Волжский, 2022 с.";
+
+        List<Editor> editors = new List<Editor>();
+        editors.Add(new Editor(){Name = "В. М. Шумячера"});
+
+        var result = StudyGuideParser.GetEditor(citation);
+
+        Assert.Equal(editors.Count, result.Count);
+        for (int i = 0; i < editors.Count; i++)
+        {
+            Assert.Equal(editors[i].Name, result[i].Name);
+        }
+    }
+    
+    [Fact]
+    public void GetEditor_MultipleEditorsTest()
+    {
+        var citation = "Внешнеэкономическая деятельность: учеб. пособие / О.Е. Акимова, И.В. Аракелова, А.А. Безлепкина, Е.Э. Головчанская, А.Ф. Джинджолия, И.В. Днепровская, Ю.И. Дубова, А.Ю. Заруднева, Л.Л. Заушицына, А.С. Иванов, В.А. Кабанов, О.Е. Малых, Я.С. Матковская, И.А. Морозова, Д. Тертри, Ж.-Л. Трюэль, А.А. Хрысева, Е.М. Черноуцан, Л.С. Шаховская; под общ. ред. Л. С. Шаховской, А. Ф. Джинджолия, И. А. Морозовой ; ВолгГТУ. - Волгоград ; Москва, 2019. - 307 с";
+
+        List<Editor> editors = new List<Editor>();
+        editors.Add(new Editor(){Name = "Л. С. Шаховской"});
+        editors.Add(new Editor(){Name = "А. Ф. Джинджолия"});
+        editors.Add(new Editor(){Name = "И. А. Морозовой"});
+
+
+
+        var result = StudyGuideParser.GetEditor(citation);
+
+        Assert.Equal(editors.Count, result.Count);
+        for (int i = 0; i < editors.Count; i++)
+        {
+            Assert.Equal(editors[i].Name, result[i].Name);
+        }
+        
+    }
+    
+    [Fact]
+    public void GetUniversities_SimpleTest()
+    {
+        var citation = "Введение в направление: учеб. пособие / Е.Ю. Силаева, Е.Л. Еремина; ВПИ (филиал) ВолгГТУ. - Волжский, 2019 с.";
+
+        List<University> universities = new List<University>();
+        universities.Add(new University(){Name = "ВПИ (филиал) ВолгГТУ"});
+
+        var result = StudyGuideParser.GetUniversity(citation);
+
+        Assert.Equal(universities.Count, result.Count);
+        for (int i = 0; i < universities.Count; i++)
+        {
+            Assert.Equal(universities[i].Name, result[i].Name);
+        }
+    }
+    
+    
+}
