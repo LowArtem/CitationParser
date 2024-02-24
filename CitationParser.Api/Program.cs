ce6721a77c3bae4de067b9fd7278b3c57dc338e4
SedovAlexandr <sedovaleksandr808@gmail.com>
2024-02-24 09:15:04 +0300

    переделал старт программы

--- CitationParser.Api/Program.cs
+++ CitationParser.Api/Program.cs
@@ -1,58 +1,28 @@
 using System.Reflection;
+using AutoMapper;
+using CitationParser.Api.Extensions.Application;
+using CitationParser.Api.Mappers;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Serilog;
 
-namespace CitationParser.Api;
+var builder = WebApplication.CreateBuilder();
 
-public class Program
-{
-    /// <summary>
-    /// Точка входа
-    /// </summary>
-    /// <param name="args"></param>
-    public static void Main(string[] args)
-    {
-        // Создание хоста
-        CreateHost(args);
-    }
+builder.Services.AddBaseModuleDi("DefaultConnection", builder.Configuration);
 
-    /// <summary>
-    /// Создание хоста
-    /// </summary>
-    /// <param name="args"></param>
-    private static void CreateHost(string[] args)
-    {
-        try
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-        catch (Exception ex)
-        {
-            Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
-            throw;
-        }
-    }
+// Auto Mapper Configurations
+var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingProfile()); });
 
-    /// <summary>
-    /// Конфиг хоста
-    /// </summary>
-    /// <param name="args"></param>
-    /// <returns></returns>
-    public static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            })
-            .ConfigureAppConfiguration(configuration =>
-            {
-                configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-                configuration.AddJsonFile(
-                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
-                    optional: true);
-            })
-            .UseSerilog((ctx, lc) => lc
-                .WriteTo.Console()
-                .ReadFrom.Configuration(ctx.Configuration));
-    }
-}
+var mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+builder.Host.UseSerilog((ctx, lc) =>lc
+    .WriteTo.Console()
+    .ReadFrom.Configuration(ctx.Configuration));
+
+var app = builder.Build();
+
+app.MigrateDatabase(app.Logger);
+
+app.UseBaseServices(app.Environment, app.Services.GetRequiredService<IApiVersionDescriptionProvider>());
+
+app.UseSerilogRequestLogging();
