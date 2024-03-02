a2cc33d61cf5e46b0c3fff07923ed18805818f50
SedovAlexandr <sedovaleksandr808@gmail.com>
2024-03-02 15:18:50 +0300

    добавил hangfire и некоторые сервисы (пытаюсь сделать как в .net 6)

--- CitationParser.Api/Program.cs
+++ CitationParser.Api/Program.cs
@@ -5,6 +5,10 @@ using CitationParser.Api.Mappers;
 using CitationParser.Data.Context;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Serilog;
+using Hangfire;
+using Hangfire.MySql;
+using CitationParser.Api.Services;
+using CitationParser.Data.Services.WebScraper;
 
 var builder = WebApplication.CreateBuilder();
 
@@ -15,6 +19,30 @@ var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingProf
 
 var mapper = mapperConfig.CreateMapper();
 builder.Services.AddSingleton(mapper);
+
+builder.Services.AddTransient<WebScraperService>();
+builder.Services.AddTransient<ParseHtmlService>();
+builder.Services.AddHostedService<TimeHostedService>();
+
+#region Hangfire
+
+// Add Hangfire services
+builder.Services.AddHangfire(configuration => configuration
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseStorage(new MySqlStorage(Configuration.GetConnectionString("HangfireConnection"),
+        new MySqlStorageOptions
+        {
+            PrepareSchemaIfNecessary = true,
+            TablesPrefix = "hangfire_"
+        }))
+);
+
+// Add the processing server as IHostedService
+builder.Services.AddHangfireServer();
+
+#endregion
 
 builder.Host.UseSerilog((ctx, lc) =>lc
     .WriteTo.Console()
