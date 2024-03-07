243babf67995d5d6893678a1f07b3b3769337f7d
SedovAlexandr <sedovaleksandr808@gmail.com>
2024-03-07 12:35:26 +0300

    рестайл

--- CitationParser.Data/Services/WebScraper/ParseHtmlService.cs
+++ CitationParser.Data/Services/WebScraper/ParseHtmlService.cs
@@ -14,10 +14,12 @@ public class ParseHtmlService
     public List<string> GetPublications(string html)
     {
         var publicationString = html.Split("id=\"LIST\"")[1].Split("</div>")[0];
-        publicationString = publicationString.Remove(0, publicationString.IndexOf('>')); // Получаем список тегов p
-        publicationString =
-            new Regex(@"<b>.*</b>").Replace(publicationString,
-                "")                         // выпиливаем все теги b с содержимым (в них просто дублируется автор)
+        
+        publicationString = publicationString
+            .Remove(0, publicationString.IndexOf('>')); // Получаем список тегов p
+        
+        publicationString = new Regex(@"<b>.*</b>")
+                .Replace(publicationString, "")   // выпиливаем все теги b с содержимым (в них просто дублируется автор)
                 .Replace("</p>", "");   // убираем все закрывающие теги p
 
         var publicationList = publicationString.Split("<p>"); // получаем список непосредственно публикаций
