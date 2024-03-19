69d9ee0693bb5513d2a31205c98bc6683863cb3f
SedovAlexandr <sedovaleksandr808@gmail.com>
2024-03-19 11:13:36 +0300

    исправил парсинг из html

--- CitationParser.Data/Services/WebScraper/ParseHtmlService.cs
+++ CitationParser.Data/Services/WebScraper/ParseHtmlService.cs
@@ -13,14 +13,20 @@ public class ParseHtmlService
     /// <returns></returns>
     public List<string> GetPublications(string html)
     {
-        var publicationString = html.Split("id=\"LIST\"")[1].Split("</div>")[0];
+        var publicationString = html.Split("ID=\"LIST\"")[1].Split("</DIV>")[0];
         
         publicationString = publicationString
-            .Remove(0, publicationString.IndexOf('>')); // Получаем список тегов p
-        
+            .Remove(0, publicationString.IndexOf('>') + 1); // Получаем список тегов p
+
+        publicationString = publicationString.Trim('\n', '\r', ' ');
+
+        if (!publicationString.Contains("<p>")) // если в списке нет статей, возвращаем пустой лист
+            return new List<string>();
+
         publicationString = new Regex(@"<b>.*</b>")
-                .Replace(publicationString, "")   // выпиливаем все теги b с содержимым (в них просто дублируется автор)
-                .Replace("</p>", "");   // убираем все закрывающие теги p
+            .Replace(publicationString, "") // выпиливаем все теги b с содержимым (в них просто дублируется автор)
+            .Replace("</p>", "") // убираем все закрывающие теги p
+            .Remove(0, 3);
 
         var publicationList = publicationString.Split("<p>"); // получаем список непосредственно публикаций
         
